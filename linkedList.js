f790e9c05da543fd97be167fecf8e314d4835528
Jose A Peralta <japc1802@hotmail.com>
2022-12-27 10:08:49 -0500

    add getIndex method add insert method

--- linkedList.js
+++ linkedList.js
@@ -46,6 +46,27 @@ class MySinglyLinkedList {
     this.length++;
     return this.length;
   }
+
+  getIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+
+  insert(index, value) {
+    if (index >= this.length) this.append(value);
+
+    const newNode = new Node(value);
+    const firstPointer = this.getIndex(index - 1);
+    const holdingPointer = firstPointer.next;
+
+    firstPointer.next = newNode;
+    newNode.next = holdingPointer;
+  }
 }
 
 let myLinkedList = new MySinglyLinkedList(1);
@@ -55,5 +76,7 @@ console.log(myLinkedList.append(3));
 console.log(myLinkedList.append(4));
 
 console.log(myLinkedList.prepend(10));
+
+console.log(myLinkedList.insert(1, 15));
 
 console.log(myLinkedList);
