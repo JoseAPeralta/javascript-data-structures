7555cde759ba9c0b5ae848243770c1a9559ec91e
Jose A Peralta <japc1802@hotmail.com>
2022-12-28 10:42:33 -0500

    create stack add peek method to stack add push method to stack

---
+++ stacks.js
@@ -0,0 +1,39 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+
+  peek() {
+    return this.top;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      const holdingPointer = this.top;
+      this.top = newNode;
+      this.top.next = holdingPointer;
+    }
+
+    this.length++;
+  }
+}
+
+const myStack = new Stack();
+
+myStack.push(10);
+myStack.push(15);
+
+console.log(myStack);
