9ae845347d0c7b61424da5da903d70051b881005
Jose A Peralta <japc1802@hotmail.com>
2022-12-28 11:18:43 -0500

    add dequeue method to queue

--- queue.js
+++ queue.js
@@ -26,6 +26,19 @@ class Queue {
       this.last = newNode;
     }
     this.length++;
+    return this;
+  }
+
+  dequeue() {
+    if (this.length === 0) return;
+    if (this.length === 1) {
+      this.first = null;
+      this.last = null;
+    } else {
+      this.first = this.first.next;
+    }
+    this.length--;
+    return this;
   }
 }
 
@@ -34,6 +47,10 @@ const myQueue = new Queue();
 myQueue.enqueue(10);
 myQueue.enqueue(15);
 myQueue.enqueue(20);
+myQueue.enqueue(25);
 
 console.log(myQueue.peek());
+myQueue.dequeue();
+console.log(myQueue.peek());
+
 console.log(myQueue);
