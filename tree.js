0674da2371c1ccbccedbf0f34605fc2c523704cc
Jose A Peralta <japc1802@hotmail.com>
2022-12-29 11:18:43 -0500

    create tree add insert method to tree

---
+++ tree.js
@@ -0,0 +1,51 @@
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (value < currentNode.value) {
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+    return this;
+  }
+}
+
+const myTree = new BinarySearchTree();
+
+myTree.insert(10);
+myTree.insert(20);
+myTree.insert(5);
+myTree.insert(2);
+myTree.insert(8);
+myTree.insert(3);
+myTree.insert(18);
+myTree.insert(20);
+console.log(myTree.root);
